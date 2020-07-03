cfe9078d382aa2edb3ca76ac4ef09ee4edf66b13
Lorna Kamau <kamaulorna@gmail.com>
2020-07-03 11:52:29 +0300

    add image model methods

--- clone/models.py
+++ clone/models.py
@@ -9,5 +9,26 @@ class Image(models.Model):
     post_date = models.DateTimeField(auto_now_add=True)
     profile = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name="posts")
-
     
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_caption(self, new_caption):
+        self.caption = new_caption
+        self.save()
+
+    def like_count(self):
+        return self.likes.count()
+
+    @classmethod
+    def get_profile_images(cls,profile):
+        return cls.objects.filter(profile = profile)
+
+    class Meta:
+        ordering = ['-post_date']
