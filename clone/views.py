66c2379c4e459d839cd941ea5f2c56e6c184cf23
Lorna Kamau <kamaulorna@gmail.com>
2020-07-08 08:52:52 +0300

    update profile url to accept user id

--- clone/views.py
+++ clone/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Image, Profile, Comment
-from .forms import UploadImageForm, EditBioForm
+from .forms import UploadImageForm, EditBioForm, FollowForm, UnfollowForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
